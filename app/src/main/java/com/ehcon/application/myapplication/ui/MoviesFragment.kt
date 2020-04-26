d6f59f4182981c7e63f2fde6ccfe8833e43a2e9e
Eh <kokominehhser@gmail.com>
2020-04-26 04:16:46 -0500

    First commit, set up navigation component

---
+++ app/src/main/java/com/ehcon/application/myapplication/ui/MoviesFragment.kt
@@ -0,0 +1,34 @@
+package com.ehcon.application.myapplication.ui
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.ehcon.application.myapplication.R
+import com.ehcon.application.myapplication.viewmodel.MoviesViewModel
+
+class MoviesFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MoviesFragment()
+    }
+
+    private lateinit var viewModel: MoviesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.movies_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MoviesViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
